ba73713eef26380ded397ce26448003f4e48ff57
b1m4s3n4 <64942799+bimasena321@users.noreply.github.com>
2021-12-25 19:39:44 +0700

    Update b1m4.py

--- b1m4.py
+++ b1m4.py
@@ -3,7 +3,7 @@ from googletrans import Translator, LANGUAGES
 translator = Translator()
 text = input("masukan text: ")
 bahasa = input("terjemahkan ke bahasa: ")
-hasil = trannslator.translate(text, dest = bahasa)
+hasil = translator.translate(text, dest = bahasa)
 
 print("dari",LANGUAGES[hasil.src],":".text)
 print("ke",LANGUAGES[hasil.dest],":",hasil.text)
